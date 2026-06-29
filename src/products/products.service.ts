0e431032fce33e61352965e28516efce78f9195e
NachoFerreyra <nachoman2030@gmail.com>
2026-06-29 15:01:27 -0300

    feat: implement products GET endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'Mouse Gamer',
+        price: 15000,
+      },
+      {
+        id: 2,
+        name: 'Teclado Mecánico',
+        price: 35000,
+      },
+    ];
+  }
+}
